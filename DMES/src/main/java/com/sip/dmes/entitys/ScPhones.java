f095f3b96ecf2923b722c772aed6708ae14cf45e
chaparro.cuadros <chaparro.cuadros@gmail.com>
2014-10-21 21:54:53 +0000

    Se agrego la informacion necesario para CRUD de personas y todas las entidades relacionadas.

--- DMES/src/main/java/com/sip/dmes/entitys/ScPhones.java
+++ DMES/src/main/java/com/sip/dmes/entitys/ScPhones.java
@@ -9,6 +9,8 @@ import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -17,27 +19,32 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.persistence.SequenceGenerator;
 
 /**
  *
  * @author gchavarro88
  */
 @Entity
-@Table(name = "sc_phones")
+@Table(name = "sc_phones" ,schema = "dmes")
 @NamedQueries(
-{
-    @NamedQuery(name = "ScPhones.findAll", query = "SELECT s FROM ScPhones s"),
-    @NamedQuery(name = "ScPhones.findByIdPhone", query = "SELECT s FROM ScPhones s WHERE s.idPhone = :idPhone"),
-    @NamedQuery(name = "ScPhones.findByNumberPhone", query = "SELECT s FROM ScPhones s WHERE s.numberPhone = :numberPhone"),
-    @NamedQuery(name = "ScPhones.findByDescription", query = "SELECT s FROM ScPhones s WHERE s.description = :description")
-})
-public class ScPhones implements Serializable
-{
+        {
+            @NamedQuery(name = "ScPhones.findAll", query = "SELECT s FROM ScPhones s"),
+            @NamedQuery(name = "ScPhones.findByIdPhone", query = "SELECT s FROM ScPhones s WHERE s.idPhone = :idPhone"),
+            @NamedQuery(name = "ScPhones.findByNumberPhone", query = "SELECT s FROM ScPhones s WHERE s.numberPhone = :numberPhone"),
+            @NamedQuery(name = "ScPhones.findByDescription", query = "SELECT s FROM ScPhones s WHERE s.description = :description")
+        })
+public class ScPhones implements Serializable {
+
+    
+
     private static final long serialVersionUID = 1L;
     @Id
     @Basic(optional = false)
     @NotNull
     @Column(name = "id_phone")
+    @GeneratedValue(generator = "dmes.sqscphones")
+    @SequenceGenerator(name = "dmes.sqscphones", sequenceName = "dmes.sqscphones", allocationSize = 1)
     private Long idPhone;
     @Basic(optional = false)
     @NotNull
@@ -50,89 +57,73 @@ public class ScPhones implements Serializable
     @ManyToOne(optional = false)
     private ScPerson idPerson;
 
-    public ScPhones()
-    {
+    public ScPhones() {
     }
 
-    public ScPhones(Long idPhone)
-    {
+    public ScPhones(Long idPhone) {
         this.idPhone = idPhone;
     }
 
-    public ScPhones(Long idPhone, long numberPhone)
-    {
+    public ScPhones(Long idPhone, long numberPhone) {
         this.idPhone = idPhone;
         this.numberPhone = numberPhone;
     }
 
-    public Long getIdPhone()
-    {
+    public Long getIdPhone() {
         return idPhone;
     }
 
-    public void setIdPhone(Long idPhone)
-    {
+    public void setIdPhone(Long idPhone) {
         this.idPhone = idPhone;
     }
 
-    public long getNumberPhone()
-    {
+    public long getNumberPhone() {
         return numberPhone;
     }
 
-    public void setNumberPhone(long numberPhone)
-    {
+    public void setNumberPhone(long numberPhone) {
         this.numberPhone = numberPhone;
     }
 
-    public String getDescription()
-    {
+    public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description)
-    {
+    public void setDescription(String description) {
         this.description = description;
     }
 
-    public ScPerson getIdPerson()
-    {
+    public ScPerson getIdPerson() {
         return idPerson;
     }
 
-    public void setIdPerson(ScPerson idPerson)
-    {
+    public void setIdPerson(ScPerson idPerson) {
         this.idPerson = idPerson;
     }
 
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         int hash = 0;
         hash += (idPhone != null ? idPhone.hashCode() : 0);
         return hash;
     }
 
     @Override
-    public boolean equals(Object object)
-    {
+    public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof ScPhones))
-        {
+        if (!(object instanceof ScPhones)) {
             return false;
         }
         ScPhones other = (ScPhones) object;
-        if ((this.idPhone == null && other.idPhone != null) || (this.idPhone != null && !this.idPhone.equals(other.idPhone)))
-        {
+        if ((this.idPhone == null && other.idPhone != null) || (this.idPhone != null && !this.idPhone.equals(other.idPhone))) {
             return false;
         }
         return true;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "com.sip.dmes.entitys.ScPhones[ idPhone=" + idPhone + " ]";
     }
-    
+
 }
